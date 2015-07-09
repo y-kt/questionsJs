8c9ba0f66777b53e4f5f6587ec41c0e998a2ca0e
Sung Kim <hunkim@gmail.com>
2015-07-09 19:08:27 +0800

    Minor update

--- js/app.js
+++ js/app.js
@@ -13,12 +13,15 @@ todomvc.filter('todoFilter', function ($location) {
 	return function (input) {
 		var sorted = [];
 		var newQuestions = [];
+		var sortedCount = 0;
+
 		angular.forEach(input, function (todo) {
 				if (todo.timestamp > new Date().getTime() - 180000) {
 					todo.new = true;
 					newQuestions.push(todo);
-				} else {
-				   sorted.push(todo);
+				} else if (sortedCount++<100){  // show top 100 only.
+					todo.new = false;
+				  sorted.push(todo);
         }
 		});
 		return newQuestions.concat(sorted);
