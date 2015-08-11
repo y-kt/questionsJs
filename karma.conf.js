5635193a8ee5ff4e22f3a86c961575eeb467822d
Sung Kim <hunkim@Sungs-MacBook-Pro.local>
2015-08-11 14:46:59 +0800

    Added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -33,13 +33,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'js/*.js': 'coverage',
+      'js/controllers/*.js': 'coverage',
+      'js/filters/*.js': 'coverage',
+      'js/directives/*.js': 'coverage'
     },
 
 
+     coverageReporter: {
+        type : 'html',
+        dir : 'coverage/'
+    },
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
--- readme.md
+++ readme.md
@@ -1,14 +1,18 @@
 Inspired by Firebase & AngularJS TodoMVC Example
 
+Local webserver:
+   yeoman server (npm install -g yo) 
+
 Unit Testing (Using Karma) - http://karma-runner.github.io/0.12/index.html
   Test case: test/unit/*
 
   Run: $ karma start karma.conf.js
 
   Installation:
-    npm install karma
+    sudo npm install -g karma  (install node first if no npm installed.)
     sudo npm install -g karma-cli
-    sugo npm install -g karma-chrome-launcher
+    sudo npm install -g karma-coverage
+    sudo npm install -g karma-chrome-launcher
     sudo npm install -g karma-jasmine
 
 E2E Testing (Using Protractor) - http://www.protractortest.org/#/
